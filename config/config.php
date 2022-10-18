56c6a355358a3f96f9e1cabb0ccc56330fe9f97c
unknown <202080480@psu.palawan.edu.ph>
2022-10-18 15:03:29 +0800

    records-app done

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/recordsapp');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '@Czarlett3');
+	define('DB_NAME', 'recordsapp');
---
+++ faker/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'razcelett/logapp-rancap',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c655f2a82dd09fee87b9d5de03588306e13d72a9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fzaninotto/faker' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'reference' => 'd0190b156bcca848d401fb80f31f504f37141c8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fzaninotto/faker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'razcelett/logapp-rancap' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c655f2a82dd09fee87b9d5de03588306e13d72a9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
